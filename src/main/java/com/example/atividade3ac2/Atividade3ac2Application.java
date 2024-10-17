c871a1bcc7ed42728d611401476927d87c2d5e24
FelipeCherry3 <felipeelias609@hotmail.com>
2024-10-17 19:13:45 -0300

    init

---
+++ src/main/java/com/example/atividade3ac2/Atividade3ac2Application.java
@@ -0,0 +1,13 @@
+package com.example.atividade3ac2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Atividade3ac2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Atividade3ac2Application.class, args);
+	}
+
+}
